0303d20466e3357eef797442fe0077bdb5da09ce
Johannes Tischer <johannes.tischer@exxeta.com>
2019-02-07 23:08:35 +0100

    added string transformation

--- src/CSVTransformator.js
+++ src/CSVTransformator.js
@@ -1,5 +1,5 @@
 const CSV_COLUMN_SPLIT_CHARACTER = ';';
-const CSV_NEWLINE_SPLIT_CHAR = '\n';
+const CSV_NEWLINE_SPLIT_CHAR = '\r\n';
 
 exports.csvToJS = function (csvContent) {
     let data = {
@@ -21,6 +21,8 @@ exports.csvToJS = function (csvContent) {
 };
 
 function extractRows(csvData) {
+    //transform input
+    csvData.replace("\r\n", "\n").replace("\r", "\n")
     return csvData.split(CSV_NEWLINE_SPLIT_CHAR);
 }
 
